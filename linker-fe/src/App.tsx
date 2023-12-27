cbf898827b3ceca2c8933397212332b7fa10ce91
Jiří Špác <capajj@gmail.com>
2023-12-27 08:33:57 +0100

    add migrations and solidjs frontend

---
+++ linker-fe/src/App.tsx
@@ -0,0 +1,19 @@
+import rrLogo from '/logo.png'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = createSignal(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={rrLogo} class="logo" alt="Rick rollers logo" />
+        </a>
+      </div>
+      <h1>Rick rollers WIP</h1>
+    </>
+  )
+}
+
+export default App
